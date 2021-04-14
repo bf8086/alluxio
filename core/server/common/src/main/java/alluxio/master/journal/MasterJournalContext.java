a7b49ba202489fc6548b0f6d40f92a8962058ca7
bf8086 <binfeng@alluxio.com>
2021-04-14 11:22:59 -0700

    Handle step down in journal writer

--- core/server/common/src/main/java/alluxio/master/journal/MasterJournalContext.java
+++ core/server/common/src/main/java/alluxio/master/journal/MasterJournalContext.java
@@ -15,12 +15,15 @@ import alluxio.ProcessUtils;
 import alluxio.conf.PropertyKey;
 import alluxio.conf.ServerConfiguration;
 import alluxio.exception.JournalClosedException;
+import alluxio.exception.status.AlluxioStatusException;
+import alluxio.exception.status.CancelledException;
 import alluxio.exception.status.UnavailableException;
 import alluxio.proto.journal.Journal.JournalEntry;
 import alluxio.retry.RetryPolicy;
 import alluxio.retry.TimeoutRetry;
 
 import com.google.common.base.Preconditions;
+import io.grpc.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -75,6 +78,11 @@ public final class MasterJournalContext implements JournalContext {
         mAsyncJournalWriter.flush(mFlushCounter);
         return;
       } catch (IOException e) {
+        if (e instanceof AlluxioStatusException
+            && ((AlluxioStatusException) e).getStatusCode() == Status.Code.CANCELLED) {
+          throw new UnavailableException(String.format("Failed to complete request: %s",
+              e.getMessage()), e);
+        }
         LOG.warn("Journal flush failed. retrying...", e);
       } catch (JournalClosedException e) {
         throw new UnavailableException(String.format("Failed to complete request: %s",
